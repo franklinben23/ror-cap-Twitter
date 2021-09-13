70a53da84f607752c15cd0c5b9dbdf18e3eef4a3
franklin <franklinbenjamin032@gmail.com>
2021-09-13 16:46:14 -0400

    create likes model, add basic follow asociation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ApplicationRecord
+  validates :body, presence: true, length: { maximum: 170-,
+  too_long: '170 characters in comment is the maximum allowed.' }
+
   belongs_to :user
+  has_many :likes, dependent: :destroy
 end
